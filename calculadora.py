aa1d7dc3714a5860c9798f274178fc435f63aea5
Daniel <danieljgc18@gmail.com>
2023-08-12 10:54:26 -0500

    Se añade la función de suma

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+print("Bienvenido a la calculadora:")
+num1 = int(input("Inserta el primer número: "))
+num2 = int(input("Inserta el segundo número: "))
+operador = input("¿Que operación deseas realizar (inserta solo el simbolo: +, -, *, /)?")
+
+if operador == "+":
+    resultado = num1 + num2
+    print(resultado)
